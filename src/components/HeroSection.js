b71bfa61ef5cfcabe28ec52911f4a570f3d161ab
saraj09 <12001826@students.koi.edu.au>
2022-05-18 04:16:59 +1000

    images updated

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='videos/video-2.mp4' autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
@@ -21,7 +21,7 @@ function HeroSection() {
           className='btns'
           buttonStyle='btn--primary'
           buttonSize='btn--large'
-          onClick={console.log('hey')}
+          //onClick={console.log('hey')}
         >
           WATCH TRAILER <i className='far fa-play-circle' />
         </Button>
