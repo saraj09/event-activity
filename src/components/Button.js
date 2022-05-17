9563a369cc4a515a66dfa453047961867c8ce39d
saraj09 <12001826@students.koi.edu.au>
2022-05-17 19:38:54 +1000

    buttons added

--- src/components/Button.js
+++ src/components/Button.js
@@ -9,11 +9,11 @@ export const Button = ({children, type, onClick, buttonStyle, buttonSize}) =>
 { 
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
    
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
         <Link to='/sign-up' className='btn-mobile'>
-            <button className={'btn ${checkButtonStyle} ${checkButtonSize}'} onClick = {onClick} type = {type}>
+            <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type = {type}>
                 {children}
             </button>
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from './Button';
 import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar() {
     const[click,setClick] = useState(false);
@@ -11,7 +11,7 @@ function Navbar() {
     const closeMobileMenu = () => setClick(false);
 
     const showButton = () => {
-        if(window.innerWidth <= 960){
+        if (window.innerWidth <= 960){
             setButton(false);
         }else{
             setButton(true);
