9e13698422cacb4cfcb57ea01ddeec825ae025dc
saraj09 <12001826@students.koi.edu.au>
2022-05-17 19:18:21 +1000

    Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
     const[click,setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
   return (
     <>
         <nav className='navbar'>
@@ -33,6 +47,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
 
         </nav>
